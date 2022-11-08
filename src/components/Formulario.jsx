5ed65835d7f05a8eeabf394fe8bc50f46a072362
Sebastian <seba.laca@gmail.com>
2022-11-08 19:20:53 +0100

    last commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,96 @@
+import { useState } from "react"
+import { Button, Col, Form, Row, Alert } from "react-bootstrap"
+import useBebidas from "../hooks/useBebidas"
+import useCategorias from "../hooks/useCategorias"
+
+
+const Formulario = () => {
+
+    const [busqueda, setBusqueda] = useState({
+        nombre:'',
+        categoria:''
+    })
+
+    const [alerta, setAlerta] = useState('')
+
+    const {categorias} = useCategorias()
+    const {consultarBebida} = useBebidas()
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        if(Object.values(busqueda).includes('')){
+            setAlerta('Todos los campos son obligatorios')
+            return
+        }
+        setAlerta('')
+        consultarBebida(busqueda)
+    }
+
+    return (
+        <Form
+            onSubmit={handleSubmit}
+        >
+            {alerta && <Alert variant="danger" className="text-center">{alerta}</Alert>}
+
+            <Row>
+                <Col md={6}>
+                    <Form.Group className="mb-3">
+                        <Form.Label htmlFor="nombre">Nombre Bebida</Form.Label>
+
+                        <Form.Control 
+                            id="nombre"
+                            type="text"
+                            placeholder="Ej: Tequila, Vodka, etc"
+                            name="nombre"
+                            value={busqueda.nombre}
+                            onChange={e => setBusqueda({
+                                ...busqueda,
+                                [e.target.name] : e.target.value
+                            })}
+                        />
+                    </Form.Group>
+                </Col>
+                <Col md={6}>
+                    <Form.Group className="mb-3">
+                        <Form.Label htmlFor="categoria">Categoría Bebida</Form.Label>
+
+                        <Form.Select
+                            id="categoria"
+                            name="categoria"
+                            value={busqueda.categoria}
+                            onChange={e => setBusqueda({
+                                ...busqueda,
+                                [e.target.name] : e.target.value
+                            })}
+                        >
+                            <option> - Selecciona Categoria - </option>
+                            {categorias.map(categoria => (
+                                <option
+                                    key={categoria.strCategory}
+                                    value={categoria.strCategory}
+                                >
+                                    {categoria.strCategory}
+                                </option>
+                            ))}
+                        </Form.Select>
+                    </Form.Group>
+                </Col>
+            </Row>
+
+            <Row className="justify-content-end"> {/* coloca el boton a la derecha */}
+                <Col md={3}>
+                    <Button
+                        variant="danger"
+                        className="text-uppercase w-100"
+                        type="submit"
+                    >
+                        Buscar Bebidas
+                    </Button>
+                </Col>
+            </Row>
+        </Form>
+    )
+}
+
+export default Formulario
---
+++ src/components/ModalBebida.jsx
@@ -0,0 +1,45 @@
+import { Image, Modal } from "react-bootstrap"
+import useBebidas from "../hooks/useBebidas"
+
+const ModalBebida = () => {
+
+    const {modal, handleModalClick, receta, cargando} = useBebidas()
+    console.log(receta)
+
+    const mostrarIngredientes = () => {
+        let ingredientes = []
+
+        for(let i=1; i < 16; i++) {
+            if(receta[`strIngredient${i}`]){
+                ingredientes.push(
+                    <li>{receta[`strIngredient${i}`]} - {receta[`strMeasure${i}`]}</li>
+                )
+            }
+        }
+        return ingredientes
+    }
+
+    return (
+        !cargando && (
+            <Modal show={modal} onHide={handleModalClick}>
+                <Image 
+                    src={receta.strDrinkThumb}
+                    alt={`imagen receta ${receta.strDrink}`}
+                />
+                <Modal.Header>
+                    <Modal.Title>{receta.strDrink}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className="p-3">
+                        <h2>Intrucciones</h2>
+                        {receta.strInstructions}
+                        <h2>Ingredientes y Cantidades</h2>
+                        {mostrarIngredientes()}
+                    </div>
+                </Modal.Body>
+            </Modal>
+        )
+    )
+}
+
+export default ModalBebida
